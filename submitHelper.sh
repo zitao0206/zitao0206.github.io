147acdfdac0d1293d9904c79b87fc0a1429cc3b5
lizitao <634376133@qq.com>
2023-08-17 22:13:46 +0200

    脚本修改

--- submitHelper.sh
+++ submitHelper.sh
@@ -5,14 +5,14 @@ sleep 2
 git add -A
 
 echo "-------Begin-------"
-if [ ! $1 ]; then
+if [ ! "$1" ]; then
     read -r -p "Please input your commit message: " input
 else
-    input=$1
+    input="$1"
 fi
 
-if [ $input ]; then
-    git commit -am $input
+if [ "$input" ]; then
+    git commit -am "$input"
 else
     git commit -am "发布/编辑新文章"
 fi
